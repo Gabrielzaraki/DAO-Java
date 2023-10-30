fc0cbde6957862c727fd974041987c5f7cf7e2dc
Gabrielzaraki <gabriel.henrique.102@gmail.com>
2023-10-30 09:15:49 -0300

    Classe Seller e interfaces DAO Department e Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+		Date data = new Date();
+		Seller seller = new Seller(21, "Bob", "Bobmail", data, 3000.0, obj);
+		System.out.println(seller);
 	}
 
 }
